6ba36dc158a11da2a398ab542975540d2fa1388b
Ștefan MURARU <muraru.stefaan@gmail.com>
2019-06-01 16:15:11 +0300

    fixed cors

--- index.js
+++ index.js
@@ -21,11 +21,7 @@ const app = express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
-  app.use(cors({
-    origin: 'http://192.168.43.222:4200',
-    optionsSuccessStatus: 200,
-    credentials: true,
-  }));
+  app.use(cors());
 
   const dependencies = await initDependencies();
   if (!dependencies) {
